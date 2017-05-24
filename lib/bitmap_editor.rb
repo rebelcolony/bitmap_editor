d145477631029d0634eeaa75b6b2b1180fa381b8
rebelcolony <kevin@rebelcolony.com>
2017-05-24 06:55:48 +0100

    refactor and clear command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -10,6 +10,8 @@ class BitmapEditor
           puts "There is no image"
       when /[I]\s\d{1,3}\s\d{1,3}/
           create_new(line)
+      when /[C]/
+          clear
       else
           puts 'unrecognised command :('
       end
@@ -26,12 +28,19 @@ class BitmapEditor
     elsif column < 1 || column > 250
       puts "sorry values must be between 1-250"
     else
-      # create grid
-      grid = Array.new(row) {Array.new(column, "O")}
-      # puts grid[0].join("")
-      # puts grid[1].join("")
-      grid.each_with_index { |x, i| puts grid[i].join("") }
+      @row = row
+      @column = column
+      create_grid
     end
+  end
+
+  def create_grid
+    grid = Array.new(@row) {Array.new(@column, "O")}
+    grid.each_with_index { |x, i| puts grid[i].join("") }
+  end
+
+  def clear
+    create_grid
   end
 
 end
